2dfe749622814498b54882b41bd7c71e87378f11
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-18 20:24:45 -0500

    successfully created a dictionary from text file

--- Assignment2/problem1.py
+++ Assignment2/problem1.py
@@ -12,20 +12,46 @@ def openFile():
 
     return filedialog.askopenfilename()
 
-def readFile(fileName):
+def readFile(fileName, dictionary):
     with open(fileName) as fileobj:
         for i, line in enumerate(fileobj):
             if(i == 0): # gets the starting point
-                start = line
+                start = line.rstrip()
             elif(i == 1):
-                finish = line #gets the goal point
+                finish = line.rstrip() #gets the goal point
                 width = len(line) #gets the width of map
             else:  # reads the map
                 for j, char in enumerate(line): #reads map char by char
                     #TODO: create a dictionary/json/turn map into an array
-                    print (char)
+                    if(j < width):
+                        dictionary.update(createNode(i, j, int(char)))
+                    else:
+                        dictionary.update(createNode(i, j, 0))
+    dictionary['start'] = start
+    dictionary['finish'] = finish
+    return dictionary
+
+def createNode(i, j, char):
+    return {(i - 2,j): char}
+
+def createNeighborsHelper(i, j, dictionary):
+    n = {(i-1,j): dictionary[(i-1, j)], (i, j-1): dictionary[(i, j-1)], (i+1, j): dictionary[(i+1, j)], (i, j+1):dictionary[(i, j+1)]}
+    return n
+
+def createNeighbors(dictionary):
+    copy = dictionary.copy()
+    for key, value in dictionary.items():
+        if(key == 'start' or key == 'finish'):
+            copy[key] = value
+        elif(int(value) > 0):
+                copy[key] = createNeighborsHelper(key[0], key[1], dictionary)
+    return copy
 
 def main():
-    readFile(openFile())
+    dictionary = {}
+    dictionary = readFile(openFile(), dictionary)
+    dictionary = createNeighbors(dictionary)
+    
+    print(dictionary)
     
 main()
