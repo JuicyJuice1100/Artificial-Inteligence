24332d917dcab6c6d289522d5245dbb8ac3b0191
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-20 01:02:35 -0500

    went back to tuples

--- Assignment2/problem1.py
+++ Assignment2/problem1.py
@@ -8,6 +8,8 @@ import tkinter as tk
 from tkinter import filedialog
 
 
+problem = ""
+
 def openFile():
     root = tk.Tk()
     root.withdraw()
@@ -19,33 +21,32 @@ def readFile(fileName, dictionary):
         for i, line in enumerate(fileobj):
             line = line.rstrip()
             if(i == 0): # gets the starting point
-                # start = re.sub(r'[^1-9]', " ", line)
-                # start = map(int, start.split())
-                start = line
+                start = re.sub(r'[^1-9]', " ", line)
+                start = map(int, start.split())
             elif(i == 1):
-                # finish = re.sub(r'[^1-9]', " ", line) #gets the goal point
-                # finish = map(int, finish.split())
-                finish = line
+                finish = re.sub(r'[^1-9]', " ", line) #gets the goal point
+                finish = map(int, finish.split())
             else:  # reads the map
                 line = list(line)
                 for j, char in enumerate(line): #reads map char by char
                     dictionary.update(createNode(i, j, int(char)))
     dictionary['start'] = start
     dictionary['finish'] = finish
+    problem = fileName
     return dictionary
 
 def createNode(i, j, char):
-    return {str((i - 2,j)): char}
+    return {(i - 2,j): char}
 
 def createNeighborsHelper(i, j, dictionary):
     n = {}
-    if(dictionary[str((i-1, j))] != 0):
+    if(dictionary[(i-1, j)] != 0):
         n.update({(i-1, j): dictionary[i-1,j]})
-    if(dictionary[str((i, j-1))] != 0):
+    if(dictionary[(i, j-1)] != 0):
         n.update({(i, j-1): dictionary[(i, j-1)]})
-    if(dictionary[str((i+1, j))] != 0):
+    if(dictionary[(i+1, j)] != 0):
         n.update({(i+1, j): dictionary[(i+1, j)]})
-    if(dictionary[str((i, j+1))] != 0):
+    if(dictionary[(i, j+1)] != 0):
         n.update({(i, j+1): dictionary[(i, j+1)]})
     # n = {(i-1,j): dictionary[(i-1, j)], (i, j-1): dictionary[(i, j-1)], (i+1, j): dictionary[(i+1, j)], (i, j+1):dictionary[(i, j+1)]}
     return n
@@ -54,7 +55,6 @@ def createNeighbors(dictionary):
     copy = dictionary.copy()
     for key, value in dictionary.items():
         if(value > 0):
-            key = key.split()
             copy[key] = createNeighborsHelper(key[0], key[1], dictionary)
         else:
             copy.pop(key)
@@ -68,11 +68,11 @@ def main():
     # goal = tuple((1,2))
     # dictionary = {k:v for k, v in dictionary.items() if v != 0}
     dictionary = createNeighbors(dictionary)
-    graph = Graph(dictionary, directed=False)
+    graph = UndirectedGraph(dictionary)
 
     # TODO: use given utils to answer questions
-    compare_searchers(problems=[GraphProblem(str(start), str(goal), graph)],
-        header=['Algorithm', 'problem 1'],
+    compare_searchers(problems=[GraphProblem(start, goal, graph)],
+        header=['Algorithm', problem],
         searchers=[uniform_cost_search]
     )
     
