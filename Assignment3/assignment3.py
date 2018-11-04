87591e91a74b97df09e15ee956bf2d77e42ea418
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-04 16:27:52 -0600

    now create domain with filtered unary constraints

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -2,8 +2,12 @@ from search import Problem
 from csp import backtracking_search, no_inference, forward_checking, mac
 from csp import first_unassigned_variable,mrv,num_legal_values,unordered_domain_values,lcv
 from csp import CSP,usa,france,australia,NQueen,UniversalDict
+from collections import Counter
+
 import tkinter as tk
 from tkinter import filedialog
+
+import itertools
 
 def openFile():
     root = tk.Tk()
@@ -40,9 +44,14 @@ def createColumns(variable):
         columns[i] = column
     variable[1] = columns # 1 = columns
 
+def filterUnaryConstraints(domain):
+    return {item for item in domain if item.count(1) == item.count(0)}
+
 def main():
     variable = {}
     readFile(openFile(), variable)
+    domain = filterUnaryConstraints({tuple(i) for i in itertools.product([0, 1], repeat=len(variable[0]))})
+    print(domain)
     print(variable)
 
 main()
