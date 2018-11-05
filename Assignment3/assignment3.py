c4aef72f8cab19a7e1aaf56509c01bd483c0b4ee
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-04 19:56:26 -0600

    have domains and neighbors.  Need to finish contraints

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -15,15 +15,15 @@ def openFile():
 
     return filedialog.askopenfilename()
 
-def readFile(fileName, variable):
+def readFile(fileName, dictionary):
     
     with open(fileName) as fileobj:
-        createRows(fileobj, variable)
+        createRows(fileobj, dictionary)
         
-    createColumns(variable)
+    createColumns(dictionary, len(dictionary))
 
-def createRows(file, variable):
-    rows = {}
+def createRows(file, dictionary):
+    # rows = {}
     for i, line in enumerate(file):
         row = []
         line = line.rstrip()
@@ -32,29 +32,71 @@ def createRows(file, variable):
                 row.append(2) # 2 = '-'
             else:
                 row.append(int(char))
-        rows[i] = row
-    variable[0] = rows # 0 = row
+        dictionary['r' + str(i)] = row
+    # dictionary['Rows'] = rows # 0 = row
 
-def createColumns(variable):
-    columns = {}
-    for i in range(0, len(variable[0])):
+def createColumns(dictionary, length):
+    temp = dictionary.copy()
+    for i in range(0, length):
         column = []
-        for value in variable[0].values():
+        for value in temp.values():
             column.append(value[i])
-        columns[i] = column
-    variable[1] = columns # 1 = columns
+        dictionary['c' + str(i)] = column
+    # dictionary['Columns'] = columns # 1 = columns
 
 def filterUnaryConstraints(domains):
     return {item for item in domains if not (any(len(list(group)) > 2 for k, group in itertools.groupby(item))) and (item.count(1) == item.count(0))}
 
+def createDictionaryWithKeys(dictionary):
+    keyDictionary = {}
+    for key, value in enumerate(dictionary):
+        keyDictionary[key] = value
+    return keyDictionary
+    
 def constraints(A, a, B, b):
+    if(A == B):
+        return (a == b)
 
+def createNeighbors(dictionary, length):
+    neighbors = {}
+    for key in list(dictionary.keys())[:length]:
+        neighbors[key] = [k for k in list(dictionary)[:length] if k != key]
+    for key in list(dictionary.keys())[length:]:
+        neighbors[key] = [k for k in list(dictionary)[length:] if k != key]
+    return neighbors
+
+def createDomain(dictionary, domains):
+    temp = dictionary.copy()
+    for key, value in temp.items():
+        temp[key] = [domain for domain in domains if shrinkDomain(value, domain)]
+    return temp
+
+def shrinkDomain(value, domain):
+    for i, j in enumerate(value):
+        if(j != 2):
+            if(j != domain[i]):
+                return False
+            # for item in domains:
+            #     if(j != item[i]):
+            #         return False
+    return True
 
 def main():
-    variable = {}
-    readFile(openFile(), variable)
-    domains = filterUnaryConstraints({tuple(i) for i in itertools.product([0, 1], repeat=len(variable[0]))})
+    dictionary = {}
+    readFile(openFile(), dictionary)
+    length = int(len(dictionary)/2)
+    domains = filterUnaryConstraints({i for i in itertools.product([0, 1], repeat=length)})
+    neighbors = createNeighbors(dictionary,length)
+    dictionary = createDomain(dictionary, domains)
+
+    # setRowDomains(dictionary['Rows'])
+    # domain = UniversalDict(domain)
+    print(neighbors)
     print(domains)
-    print(variable)
+    print(dictionary)
+
+    problem = CSP(list(dictionary.keys()), UniversalDict(domains), neighbors, constraints)
+    result = backtracking_search(problem,select_unassigned_variable=mrv,order_domain_values=lcv,inference=mac) 
+    print(result)
 
 main()
